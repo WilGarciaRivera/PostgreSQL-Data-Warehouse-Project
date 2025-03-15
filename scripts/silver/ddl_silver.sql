ea850f6267669c5d7a9961351503fa4c6e28ef2d
WilGarciaRivera <118150758+WilGarciaRivera@users.noreply.github.com>
2025-03-14 22:01:09 -0400

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -75,13 +75,14 @@ END $$;
 
 CREATE TABLE silver.crm_prd_info (
     prd_id INT,
+    cat_id VARCHAT(50),
     prd_key VARCHAR(50),
     prd_nm VARCHAR(50),
     prd_cost INT,
     prd_line VARCHAR(50),
-    prd_start_dt TIMESTAMP,
-    prd_end_dt TIMESTAMP,
-	wgr_create_date TIMESTAMP DEFAULT NOW()
+    prd_start_dt DATE,
+    prd_end_dt DATE,
+    wgr_create_date TIMESTAMP DEFAULT NOW()
 );
 
 DO $$ 
