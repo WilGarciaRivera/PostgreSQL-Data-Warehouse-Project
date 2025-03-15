5b6e112fbcdcd9572bf8aedc246940301b959545
WilGarciaRivera <118150758+WilGarciaRivera@users.noreply.github.com>
2025-03-15 00:05:44 -0400

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -94,7 +94,8 @@ END $$;
 
 CREATE TABLE silver.erp_loc_a101 (
     cid VARCHAR(50),
-    cntry VARCHAR(50)
+    cntry VARCHAR(50),
+    wgr_create_date TIMESTAMP DEFAULT NOW()
 );
 
 DO $$ 
@@ -107,7 +108,8 @@ END $$;
 CREATE TABLE silver.erp_cust_az12 (
     cid VARCHAR(50),
     bdate DATE,
-    gen VARCHAR(50)
+    gen VARCHAR(50),
+    wgr_create_date TIMESTAMP DEFAULT NOW()
 );
 
 DO $$ 
@@ -121,5 +123,6 @@ CREATE TABLE silver.erp_px_cat_g1v2 (
     id VARCHAR(50),
     cat VARCHAR(50),
     subcat VARCHAR(50),
-    maintenance VARCHAR(50)
+    maintenance VARCHAR(50),
+    wgr_create_date TIMESTAMP DEFAULT NOW()
 );
